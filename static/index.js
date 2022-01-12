6f5c3e4a7e17f8f300287a0851c35bb7927a332d
JunJon09 <84631911+JunJon09@users.noreply.github.com>
2022-01-12 14:43:09 +0900

    戻るボタン以外のJSは完成

--- static/index.js
+++ static/index.js
@@ -30,48 +30,49 @@ let disbutton6 = document.getElementById('disbtn6');
 let disbutton7 = document.getElementById('disbtn7');
 let disbutton8 = document.getElementById('disbtn8');
 
-mybutton1.addEventListener('click', my_one_eight_buttonClick);
-mybutton2.addEventListener('click', my_one_eight_buttonClick);
-mybutton3.addEventListener('click', my_one_eight_buttonClick);
-mybutton4.addEventListener('click', my_one_eight_buttonClick);
-mybutton5.addEventListener('click', my_one_eight_buttonClick);
-mybutton6.addEventListener('click', my_one_eight_buttonClick);
-mybutton7.addEventListener('click', my_one_eight_buttonClick);
-mybutton8.addEventListener('click', my_one_eight_buttonClick);
+mybutton1.addEventListener('click', {number:'1', handleEvent:my_one_eight_buttonClick});
+mybutton2.addEventListener('click', {number:'2', handleEvent:my_one_eight_buttonClick});
+mybutton3.addEventListener('click', {number:'3', handleEvent:my_one_eight_buttonClick});
+mybutton4.addEventListener('click', {number:'4', handleEvent:my_one_eight_buttonClick});
+mybutton5.addEventListener('click', {number:'5', handleEvent:my_one_eight_buttonClick});
+mybutton6.addEventListener('click', {number:'6', handleEvent:my_one_eight_buttonClick});
+mybutton7.addEventListener('click', {number:'7', handleEvent:my_one_eight_buttonClick});
+mybutton8.addEventListener('click', {number:'8', handleEvent:my_one_eight_buttonClick});
 mybutton9.addEventListener('click',my_nine_buttonClick);
 
-yourbutton1.addEventListener('click', your_one_eight_buttonClick);
-yourbutton2.addEventListener('click', your_one_eight_buttonClick);
-yourbutton3.addEventListener('click', your_one_eight_buttonClick);
-yourbutton4.addEventListener('click', your_one_eight_buttonClick);
-yourbutton5.addEventListener('click', your_one_eight_buttonClick);
-yourbutton6.addEventListener('click', your_one_eight_buttonClick);
-yourbutton7.addEventListener('click', your_one_eight_buttonClick);
-yourbutton8.addEventListener('click', your_one_eight_buttonClick);
+yourbutton1.addEventListener('click', {number:'1', handleEvent:your_one_eight_buttonClick});
+yourbutton2.addEventListener('click', {number:'2', handleEvent:your_one_eight_buttonClick});
+yourbutton3.addEventListener('click', {number:'3', handleEvent:your_one_eight_buttonClick});
+yourbutton4.addEventListener('click', {number:'4', handleEvent:your_one_eight_buttonClick});
+yourbutton5.addEventListener('click', {number:'5', handleEvent:your_one_eight_buttonClick});
+yourbutton6.addEventListener('click', {number:'6', handleEvent:your_one_eight_buttonClick});
+yourbutton7.addEventListener('click', {number:'7', handleEvent:your_one_eight_buttonClick});
+yourbutton8.addEventListener('click', {number:'8', handleEvent:your_one_eight_buttonClick});
 yourbutton9.addEventListener('click', your_nine_buttonClick);
 
 plusInning.addEventListener('click', pulasCount);
 minusInning.addEventListener('click', minusCount);
 
-disbutton1.addEventListener('click', dis_one_eight_buttonClick);
-disbutton2.addEventListener('click', dis_one_eight_buttonClick);
-disbutton3.addEventListener('click', dis_one_eight_buttonClick);
-disbutton4.addEventListener('click', dis_one_eight_buttonClick);
-disbutton5.addEventListener('click', dis_one_eight_buttonClick);
-disbutton6.addEventListener('click', dis_one_eight_buttonClick);
-disbutton7.addEventListener('click', dis_one_eight_buttonClick);
-disbutton8.addEventListener('click', dis_one_eight_buttonClick);
-
+disbutton1.addEventListener('click', {number:'1', handleEvent:dis_one_eight_buttonClick});
+disbutton2.addEventListener('click', {number:'2', handleEvent:dis_one_eight_buttonClick});
+disbutton3.addEventListener('click', {number:'3', handleEvent:dis_one_eight_buttonClick});
+disbutton4.addEventListener('click', {number:'4', handleEvent:dis_one_eight_buttonClick});
+disbutton5.addEventListener('click', {number:'5', handleEvent:dis_one_eight_buttonClick});
+disbutton6.addEventListener('click', {number:'6', handleEvent:dis_one_eight_buttonClick});
+disbutton7.addEventListener('click', {number:'7', handleEvent:dis_one_eight_buttonClick});
+disbutton8.addEventListener('click', {number:'8', handleEvent:dis_one_eight_buttonClick});
 
 
 
 //自分の1-8までのボタンの時
-function my_one_eight_buttonClick(){
+function my_one_eight_buttonClick(e){
   // 今の値をとってくる
   let frontNumber = document.getElementById('mycount').textContent;
   let moreNumber = document.getElementById('myMore').textContent;
+  //ボタンを消す関数を出力している。
 
   if(moreNumber != "Win!!" && moreNumber!="Lose"){
+    deleteButton(this.number)
     // 文字列を数値の方に変更
     frontNumber = Number(frontNumber);
     let count = frontNumber + 1;
@@ -86,39 +87,58 @@ function my_one_eight_buttonClick(){
       document.getElementById('myMore').textContent = morecount
     }
   }
-  }
+}
 
 //自分が９を落とした時
 function my_nine_buttonClick(){
   let frontNumber = document.getElementById('mycount').textContent;
   let moreNumber = document.getElementById('myMore').textContent;
-
+  
   if(moreNumber != "Win!!" && moreNumber!="Lose"){
-    // 文字列を数値の方に変更
-    frontNumber = Number(frontNumber)
-    count = frontNumber + 2;
-    document.getElementById('mycount').textContent = count;
-    
-    //Moreのとこ
-    moreNumber = Number(moreNumber);
-    let morecount = moreNumber - 2;
-    if(morecount <= 0){
-      document.getElementById('myMore').textContent = "Win!!"
-      document.getElementById('yourMore').textContent = "Lose"
+    var reuslt = window.confirm('9を落としたので、次のラックに進みますか？');
+    if(reuslt){
+        // 文字列を数値の方に変更
+      frontNumber = Number(frontNumber)
+      count = frontNumber + 2;
+      document.getElementById('mycount').textContent = count;
+      
+      //Moreのとこ
+      moreNumber = Number(moreNumber);
+      let morecount = moreNumber - 2;
+      if(morecount <= 0){
+        document.getElementById('myMore').textContent = "Win!!"
+        document.getElementById('yourMore').textContent = "Lose"
+      }else{
+        document.getElementById('myMore').textContent = morecount
+
+        //途中で９を落とした時の無効球を考える。
+        let yourNumber = document.getElementById('yourcount').textContent;
+        yourNumber = Number(yourNumber)
+        let disNumber = document.getElementById('disball').textContent;
+        disNumber = Number(disNumber);
+        if((count+yourNumber+disNumber)%10 != 0){
+          let ball = (count+yourNumber+disNumber)%10;
+          let allBall = (10 - ball) + disNumber;
+          document.getElementById('disball').textContent = allBall;
+        }
+      }
+      openeButton()
     }else{
-      document.getElementById('myMore').textContent = morecount
+      window.alert('処理をキャンセルしました。')
     }
   }
 }
 
 //yourのアクション
 
-function your_one_eight_buttonClick(){
+function your_one_eight_buttonClick(e){
   // 今の値をとってくる
   let frontNumber = document.getElementById('yourcount').textContent;
   let moreNumber = document.getElementById('yourMore').textContent;
 
   if(moreNumber != "Win!!" && moreNumber!="Lose"){
+    //ボタンを消す関数を出力している。
+    deleteButton(this.number);
     // 文字列を数値の方に変更
     frontNumber = Number(frontNumber);
     let count = frontNumber + 1;
@@ -135,26 +155,44 @@ function your_one_eight_buttonClick(){
   }
 }
 
-//自分が９を落とした時
+//相手が９を落とした時
 function your_nine_buttonClick(){
   let frontNumber = document.getElementById('yourcount').textContent;
   let moreNumber = document.getElementById('yourMore').textContent;
 
   if(moreNumber != "Win!!" && moreNumber!="Lose"){
-    // 文字列を数値の方に変更
-    frontNumber = Number(frontNumber)
-    count = frontNumber + 2;
-    document.getElementById('yourcount').textContent = count;
-    
-    //Moreのとこ
-    moreNumber = Number(moreNumber);
-    let morecount = moreNumber - 2;
-    if(morecount <= 0){
-      document.getElementById('yourMore').textContent = "Win!!"
-      document.getElementById('myMore').textContent = "Lose"
+    var reuslt = window.confirm('9を落としたので、次のラックに進みますか？');
+    if(reuslt){
+      // 文字列を数値の方に変更
+      frontNumber = Number(frontNumber)
+      count = frontNumber + 2;
+      document.getElementById('yourcount').textContent = count;
+      
+      //Moreのとこ
+      moreNumber = Number(moreNumber);
+      let morecount = moreNumber - 2;
+      if(morecount <= 0){
+        document.getElementById('yourMore').textContent = "Win!!"
+        document.getElementById('myMore').textContent = "Lose"
+      }else{
+        document.getElementById('yourMore').textContent = morecount
+
+        //途中で９を落とした時の無効球を考える。
+        let myNumber = document.getElementById('mycount').textContent;
+        myNumber = Number(myNumber)
+        let disNumber = document.getElementById('disball').textContent;
+        disNumber = Number(disNumber);
+        if((count+myNumber+disNumber)%10 != 0){
+          let ball = (count+myNumber+disNumber)%10;
+          let allBall = (10 - ball) + disNumber;
+          document.getElementById('disball').textContent = allBall;
+        }
+      }
+      openeButton();
     }else{
-      document.getElementById('yourMore').textContent = morecount
+      window.alert('処理をキャンセルしました。');
     }
+    
   }
 }
 
@@ -179,11 +217,78 @@ function dis_one_eight_buttonClick(){
   let disNumber = document.getElementById('disball').textContent;
   let moreNumber = document.getElementById('yourMore').textContent;
 
+  //ボタンを消す関数を出力している。
+  deleteButton(this.number)
+
   if(moreNumber != "Win!!" && moreNumber!="Lose"){
     // 文字列を数値の方に変更
     disNumber = Number(disNumber);
     let count = disNumber + 1;
     document.getElementById('disball').textContent = count;
-    //Moreのとこ
   }
+}
+
+//ボタンを隠す関数
+function deleteButton(number){
+  if(number=="1"){
+    mybutton1.style.display = "none";
+    yourbutton1.style.display = "none";
+    disbutton1.style.display = "none";
+  }else if(number=="2"){
+    mybutton2.style.display = "none";
+    yourbutton2.style.display = "none";
+    disbutton2.style.display = "none";
+  }else if(number=="3"){
+    mybutton3.style.display = "none";
+    yourbutton3.style.display = "none";
+    disbutton3.style.display = "none";
+  }else if(number=="4"){
+    mybutton4.style.display = "none";
+    yourbutton4.style.display = "none";
+    disbutton4.style.display = "none";
+  }else if(number=="5"){
+    mybutton5.style.display = "none";
+    yourbutton5.style.display = "none";
+    disbutton5.style.display = "none";
+  }else if(number=="6"){
+    mybutton6.style.display = "none";
+    yourbutton6.style.display = "none";
+    disbutton6.style.display = "none";
+  }else if(number=="7"){
+    mybutton7.style.display = "none";
+    yourbutton7.style.display = "none";
+    disbutton7.style.display = "none";
+  }else if(number=="8"){
+    mybutton8.style.display = "none";
+    yourbutton8.style.display = "none";
+    disbutton8.style.display = "none";
+  }
+}
+
+//ボタンを表示する
+function openeButton(){
+  mybutton1.style.display = "";
+  yourbutton1.style.display = "";
+  disbutton1.style.display = "";
+  mybutton2.style.display = "";
+  yourbutton2.style.display = "";
+  disbutton2.style.display = "";
+  mybutton3.style.display = "";
+  yourbutton3.style.display = "";
+  disbutton3.style.display = "";
+  mybutton4.style.display = "";
+  yourbutton4.style.display = "";
+  disbutton4.style.display = "";
+  mybutton5.style.display = "";
+  yourbutton5.style.display = "";
+  disbutton5.style.display = "";
+  mybutton6.style.display = "";
+  yourbutton6.style.display = "";
+  disbutton6.style.display = "";
+  mybutton7.style.display = "";
+  yourbutton7.style.display = "";
+  disbutton7.style.display = "";
+  mybutton8.style.display = "";
+  yourbutton8.style.display = "";
+  disbutton8.style.display = "";
 }
